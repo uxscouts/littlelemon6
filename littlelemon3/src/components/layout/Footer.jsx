874e8affe579d1a227a13ea7c6ee8a8cee5c22b3
UX SCOUTS <admin@uxscouts.com>
2026-03-05 18:12:15 +0000

    footer

--- littlelemon3/src/components/layout/Footer.jsx
+++ littlelemon3/src/components/layout/Footer.jsx
@@ -1,7 +1,36 @@
 function Footer() {
   return (
 <>
-  <p className='footer'>Footer</p>
+     <div class="maxWidth-footer">
+      <div class="super-container-footer">
+        <div class="grid-container-footer">
+          <div class="MenuBox">
+            <img src="./assets/images/Footer-Logo-small.png" class="img-fluid" alt="Responsive image" width="200" />
+          </div>
+          <div class="MenuBox">
+            <h4>Navigation</h4>
+            <p>Home</p>
+            <p>About</p>
+            <p>Menu</p>
+            <p>Reservations</p>
+            <p>Order Online</p>
+            <p>Login</p>
+          </div>
+          <div class="MenuBox">
+            <h4>Contact</h4>
+            <p>69 Vico Araratiano</p>
+            <p>(709)879-4459</p>
+            <p>Email Us</p>
+          </div>
+          <div class="MenuBox">
+            <h4>Socials</h4>
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>Instagram</p>
+          </div>
+        </div>
+      </div>
+    </div>  
 </>
   )
 }
